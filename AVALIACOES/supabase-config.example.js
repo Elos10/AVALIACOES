62173b4296cae654f4ba1e970663b86e02544db2
Eder Leonardo <140769660+Elos10@users.noreply.github.com>
2026-06-09 15:54:47 -0300

    3

--- AVALIACOES/supabase-config.example.js
+++
@@ -1,2 +0,0 @@
-export const SUPABASE_URL = 'https://itenlnlbiefrxteggjsh.supabase.co';
-export const SUPABASE_ANON_KEY = 'sb_publishable_CfQZEfvPeTARVyQvJ5Cd1g_wLbqc8lV';
---
+++ supabase-config.example.js
@@ -0,0 +1,2 @@
+export const SUPABASE_URL = 'https://aluuqvuwfywijqnxjyos.supabase.co';
+export const SUPABASE_ANON_KEY = 'sb_publishable_oA4CsQXsV4aVL0wJT3gxlw_iUFcGMEY';
